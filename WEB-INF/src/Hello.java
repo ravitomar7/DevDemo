211db076cc831c95315357747e7a31015be557da
Ravi Tomar <ravitomar7@gmail.com>
2020-01-22 22:05:18 +0530

    First

---
+++ WEB-INF/src/Hello.java
@@ -0,0 +1,29 @@
+package com.rt;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import java.util.Date;
+public class Hello extends HttpServlet
+{ 
+int i;
+public void init(ServletConfig config)
+{
+i=0;
+}
+ 
+public void doGet(HttpServletRequest req,HttpServletResponse res)  
+throws ServletException,IOException  
+{  i++;
+res.setContentType("text/html");
+
+PrintWriter out=res.getWriter();
+
+out.println("<html><body>");  
+out.println("Welcome"+i+" to servlet at"+new Date());  
+out.println("</body></html>");  
+  
+out.close();
+
+}}  
+
